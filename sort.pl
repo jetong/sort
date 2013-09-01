56243955347aed19a8860d7af72ca5e1b399cc73
vagrant <vagrant@precise32.(none)>
2013-09-01 03:46:35 +0000

    initial commit

---
+++ sort.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+
+open (my $fh, "<", "data.txt") or die "could not open file: $!\n";
+
+my %names;
+
+while (my $line = <$fh>) {
+	chomp $line;
+	$names{$line} = 0;
+}
+
+my @sorted = sort {$a <=> $b} keys %names;
+print "@sorted\n\n";
+
+foreach my $key (keys %names) {
+	print "$key: $names{$key}\n";
+}
